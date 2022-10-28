495204ef25798253786057b2dff6d3f9b573e3b4
MuhammadHassanShah <hassan.hameed@netsoltech.com>
2022-10-28 22:56:23 +0500

    updated footer

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 import logo from './images/cinvoclogo.svg'; 
 import phone from './images/phone.svg'; 
 import home from './images/homeImg.svg'; 
-import Footer from './Footer'; 
+import Footer from './components/Footer/Footer'; 
 import Services from './components/Services';
+import freeconsultation from './images/freeconsultation.png';
+import makeIT from './images/makeIT.svg';
+
 
 function App() {
   return (
@@ -92,13 +95,74 @@ Satisfaction</p>
   </div>
 
 
+
+
+
+
+
 <Services />
+
+{/* make IT */}
+<div class="Container"    >
+   <img src={makeIT}    width="100%"  alt="logo" height="400px" style={{marginBottom:"-10px"}} />
+     <div class="centeredIT" style={{fontFamily: "Assistant" , fontWeight : "600" , fontSize: "40px", color:"white" ,textTransform: "capitalize" ,  top: "15%" 
+
+}}>We Make IT
+</div>
+<div class="centeredIT" style={{fontFamily: "Assistant" , fontWeight : "600" , fontSize: "40px", color:"white" ,textTransform: "capitalize" ,  top: "30%" ,marginTop: "0%"
+
+}}>Simple, Faster, And
+</div>
+<div class="centeredIT" style={{fontFamily: "Assistant" , fontWeight : "600" , fontSize: "40px", color:"white" ,textTransform: "capitalize" ,  top: "45%" ,marginTop: "0%"
+
+}}>Less Expensive!
+</div>
+<div class="centeredIT" style={{  color:"white" ,textTransform: "capitalize" ,  top: "65%" ,marginTop: "0%",width : "10%"
+
+}}><button type="button" class="btn btn-primary btn-sm"   style={{ color:"white" ,backgroundColor : "#F37024" ,borderColor : "#F37024" , width : "100%" ,  top: "60%",height :"5%",textAlign:"center" ,fontWeight : "500", borderRadius: "6px"
+}}>Learn More</button>
+</div>
+</div>
+
+
+
+{/* freeconsultation */}
+<div class="Container" >
+   <img src={freeconsultation}    width="100%"  alt="logo" height="200px" style={{backgroundColor : "#F37024" , marginBottom:"-6px"}} />
+
+  
+  <div class="centered" style={{fontFamily: "Assistant" , fontWeight : "600" , fontSize: "40px", color:"white" ,textTransform: "capitalize" ,  top: "30%"
+
+}}>there are many other things we can do
+</div>
+<div class="centered" style={{fontFamily: "Assistant" , fontWeight : "50" , fontSize: "27px", color:"white" ,textTransform: "capitalize" ,marginTop: "-5%"
+}}>get started to talk with us & get infinite services & free consultation.
+</div></div>
+
+
+
+
+{/* white area */}
+<div class="container" style={{marginTop : "70px", marginBottom : "70px"}}>
+  <div class="row">
+    <div class="col-5" style={{fontFamily: "Assistant" , fontWeight : "350" , fontSize: "25px", color:"#F37024" ,textTransform: "capitalize"
+}}>
+      build a respected relation with us!
+    </div>
+    <div class="col-5">
+     <input type="text" class="form-control" placeholder="Search" aria-label="Recipient's username" aria-describedby="basic-addon2" style={{backgroundColor : "#F0F0F0" , borderColor : "#F0F0F0"}}/>
+    </div>
+    <div class="col-2">
+<button type="button" class="btn btn-primary"   style={{ color:"white" ,backgroundColor : "#F37024" ,borderColor : "#F37024" , width : "70%" ,fontWeight : "500" ,borderRadius: "6px"
+}}>send</button>
+    </div>
+  </div>
+</div>
+
    <Footer/>
 
   </div>
-   
- 
- 
+
   );
 }
 
--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -1,12 +1,33 @@
 import React from 'react'
 import './index.css';
-import circle from '../../images/circle.png'
 import ellipse from '../../images/ellipse.png'
+import Rectangle from '../../images/Rectangle.svg'
+
 const Services = () => {
   return (
     <div>
-      
-      <img src={ellipse}  className='contain' height={"430"} width={"1440"}/>
+      <div class="Container"    >
+              <img src={Rectangle}  class="image2"  alt="logo"  />
+
+      <img src={ellipse} class="image1"  width="100%"  alt="logo" height="800px" style={{marginBottom:"-6px"}}/>
+           <div class="centeredIT" style={{fontFamily: "Assistant" , fontWeight : "600" , fontSize: "40px", color:"white" ,textTransform: "capitalize" ,  top: "2%" 
+
+}}>Our Services
+</div>
+<div class="centeredIT" style={{fontFamily: "Assistant" , fontWeight : "600" , fontSize: "40px", color:"white" ,textTransform: "capitalize" ,  top: "7%" ,marginTop: "0%"
+
+}}>Here Is Our Services We Boost Up Your
+</div> 
+<div class="centeredIT" style={{fontFamily: "Assistant" , fontWeight : "600" , fontSize: "40px", color:"white" ,textTransform: "capitalize" ,  top: "11%" ,marginTop: "0%"
+
+}}>Business!
+</div>   
+<div class="centeredIT" style={{  color:"white" ,textTransform: "capitalize" ,  top: "20%" ,marginTop: "0%",width : "7%"
+
+}}><button type="button" class="btn btn-primary btn-sm"   style={{ color:"white" ,backgroundColor : "#212222" ,borderColor : "#212222" , width : "100%" ,  top: "30%",height :"5%",textAlign:"center" ,fontWeight : "500",fontSize : "60%" ,borderRadius: "6px"
+}}>Explore More</button>
+</div>
+  </div>
       <div className='circle'>
       {/* Services */}
       </div>
